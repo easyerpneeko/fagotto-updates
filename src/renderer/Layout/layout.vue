b335e08239da15296f11ac64623ef78536a1659c
easyerpneeko <97813387+easyerpneeko@users.noreply.github.com>
2026-02-03 04:55:49 -0300

    v1.11.100 - Make Mapa de Calor available for all roles

--- src/renderer/Layout/layout.vue
+++ src/renderer/Layout/layout.vue
@@ -446,15 +446,13 @@ export default {
                 requiresTurno: true
               });
             }
-            // Mapa de Calor
-            if (reportsGestion) {
-              menu.push({
-                label: 'Mapa de Calor',
-                route: '/inicio/mapa-calor',
-                icon: 'fa-fire',
-                requiresTurno: true
-              });
-            }
+            // Mapa de Calor - Disponible para todos los roles
+            menu.push({
+              label: 'Mapa de Calor',
+              route: '/inicio/mapa-calor',
+              icon: 'fa-fire',
+              requiresTurno: true
+            });
             // Submodulos de gastos del dia
             if (expenses_day && expensesGestion) {
               menu.push({
